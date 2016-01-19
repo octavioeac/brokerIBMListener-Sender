38e29c7e91b4d272fa7acdc1c0caf59e0107843d
Octavio <Octavio@10.105.211.94>
2016-01-18 19:03:24 -0600

    MAndando Mensaje para ARM EQUIPOP

--- fridaSendARM/src/main/java/com/frida/controller/BrokerRestController.java
+++ fridaSendARM/src/main/java/com/frida/controller/BrokerRestController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.frida.controller.pojo.GeneralData;
-import com.frida.controller.pojo.Objetito;
+import com.frida.controller.pojo.Equipo;
+import com.frida.controller.pojo.MensajeEquipo;
 import com.frida.singleton.sender.singletonQueue;
 
 import www.frida.modelo.Snippet;
+import www.frida.modelo.Dao.catAnilloBusqueda;
 
 
 
@@ -42,57 +43,21 @@ public class BrokerRestController {
 	    return lastName+"HOLA MUNDO";
 	  }
 	
-	@RequestMapping(value="/prueba", method=RequestMethod.POST,
+	
+	
+	
+	@RequestMapping(value="/equipo", method=RequestMethod.POST,
 			                         consumes=MediaType.APPLICATION_JSON_VALUE,
 			                        		 produces=MediaType.APPLICATION_JSON_VALUE)
-	public Objetito getPrueba(@RequestBody GeneralData gral)throws Exception{
-		System.out.println("HOLA JSON");
-		System.out.println(gral.getNombre()+gral.getNombre());
-		Objetito k=new Objetito();
-		String cadena="<PeEquipo>"
-				+ "<ID_Nodo>"+gral.getEquipo()+"</ID_Nodo>"
-				+ "<ID_Nodo_Nuevo>"+gral.getNombre()+"</ID_Nodo_Nuevo>"
-				+ "<Movimiento>"+gral.getNumero()+"</Movimiento>"
-				+ "<CLLIEdificio>Prueba Frida para4</CLLIEdificio>"
-				+ "<UbicacionEquipo>Prueba Frida para5</UbicacionEquipo>"
-				+ "<CLLI>Prueba Frida para6</CLLI>"
-				+ "<Cluster>Prueba Frida para7</Cluster>"
-				+ "<Modelo>Prueba Frida para8</Modelo>"
-				+ "<Division>Prueba Frida para9</Division>"
-				+ "<CLEI>Prueba Frida para10</CLEI>"
-				+ "<TipoNodoTelmex>Prueba Frida para11</TipoNodoTelmex>"
-				+ "<OSPF>Prueba Frida para12</OSPF>"
-				+ "<Repisa>Prueba Frida para13</Repisa>"
-				+ "<IPSistema>Prueba Frida para14</IPSistema>"
-				+ "<IPGestion>Prueba Frida para15</IPGestion>"
-				+ "<EstadoAprovisionamiento>Prueba Frida para16</EstadoAprovisionamiento>"
-				+ "<FechaGestion>Prueba Frida para17</FechaGestion>"
-				+ "<FolioOrdenTrabajo>Prueba Frida para18</FolioOrdenTrabajo>"
-				+ "<Notas>Prueba Frida para19</Notas>"
-				+ "<VersionSistemaOperativo>Prueba Frida para20</VersionSistemaOperativo>"
-				+ "<TipoGestion>Prueba Frida para21</TipoGestion>"
-				+ "<VLANGestion>Prueba Frida para22</VLANGestion>"
-				+ "<ConexionPorTunel>Prueba Frida para23</ConexionPorTunel>"
-				+ "<SwitchRCDT>Prueba Frida para24</SwitchRCDT>"
-				+ "<PuertoRCDT>Prueba Frida para25</PuertoRCDT>"
-				+ "<Velocidad>Prueba Frida para26</Velocidad>"
-				+ "<FolioRCDT>Prueba Frida para27</FolioRCDT>"
-				+ "<PEP_Element_ID>Prueba Frida para28</PEP_Element_ID>"
-				+ "<FechaBaja>Prueba Frida para29</FechaBaja>"
-				+ "<FechaAlta>Prueba Frida para30</FechaAlta>"
-				+ "<FechaCambio>Prueba Frida para31</FechaCambio>"
-				+ "<Proveedor>Prueba Frida para32</Proveedor>"
-				+ "</PeEquipo>";
-		Snippet julio=new Snippet();
-		julio.pruebaStore();
-
-	
-
-		singletonQueue j=new singletonQueue();
-		j.mandarMensaje(cadena);
+	public MensajeEquipo sendEquipo(@RequestBody Equipo gral)throws Exception{
+		MensajeEquipo k=new MensajeEquipo()	;
 		
-		k.setNombre("HOLA MUNDO");
-		k.setRef_sisa("RFC-787-HJUI");
+		catAnilloBusqueda cadena=new catAnilloBusqueda();
+		singletonQueue j=new singletonQueue();
+		j.mandarMensaje(cadena.getPeEquipo(gral.getIdEquipo()));
+		
+		k.setMensaje("OK");
+		
 		return k;
 		
 		
